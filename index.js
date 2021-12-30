409cb4cb6baf688914c7e5959c77e8b24763c9d1
Yuri <arhipow.yuri@gmail.com>
2021-12-30 18:09:44 +0600

    add cycle

--- index.js
+++ index.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastWatchedFilm = prompt ('Один из последних просмотренных фильмов?', ''),
-        gradeFilm = prompt ('На сколько оцените его?', ''),
-        a = prompt ('Один из последних просмотренных фильмов?', ''),
-        b = prompt ('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastWatchedFilm] = gradeFilm;
-personalMovieDB.movies[a] = b;
+for (let i = 0; i < 3; i++) {
+    const lastWatchedFilm = prompt ('Один из последних просмотренных фильмов?', ''),
+        gradeFilm = prompt ('На сколько оцените его?', '');
+
+    if (lastWatchedFilm != null && gradeFilm != null && lastWatchedFilm != '' && gradeFilm != '' &&
+    lastWatchedFilm.length < 50) {       
+        personalMovieDB.movies[lastWatchedFilm] = gradeFilm;  
+        console.log('done');  
+    } else {
+        console.log('error');
+        i--;
+    };
+};
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
